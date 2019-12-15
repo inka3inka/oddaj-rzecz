2d09351727c9de609a05d1f840d8ddd33e6e8c4d
inka3inka <inka3inka@gmail.com>
2019-12-15 01:05:22 +0100

    Column's main structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Header} from './header/Header'
 import {MainSection} from "./mainSection/MainSection";
+import {ColumnsContainer} from "./threeColumnsSection/ColumnsContainer";
 
 export function Home() {
   return (
     <>
       <Header />
       <MainSection />
+      <ColumnsContainer/>
     </>
   )
 }
---
+++ src/components/threeColumnsSection/ColumnElement.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function ColumnElement(props) {
+  return (
+    <div>
+      <div>{props.number}</div>
+      <div>{props.title}</div>
+      <div>{props.text}</div>
+    </div>
+
+  )
+}
+
+//Lorem ipsum dolor sit amet, consectetur adipisc Pellentseque vel enim a elit viverra elementuma. Alquam erat volutpat.
