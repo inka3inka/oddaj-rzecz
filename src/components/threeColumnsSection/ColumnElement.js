84f7dc2cf95f7e2d92d3574322a91c6a973066f7
inka3inka <inka3inka@gmail.com>
2019-12-15 12:18:55 +0100

    Prepared Three Columns Section - without printing state information

--- src/components/threeColumnsSection/ColumnElement.js
+++ src/components/threeColumnsSection/ColumnElement.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export function ColumnElement(props) {
   return (
-    <div>
-      <div>{props.number}</div>
-      <div>{props.title}</div>
-      <div>{props.text}</div>
+    <div className="columns__element">
+      <div className="columns__element--number">{props.number}</div>
+      <div className="columns__element--title">{props.title}</div>
+      <div className="columns__element--text">{props.text}</div>
     </div>
 
   )
