fc9cf8b2ec71d810f859239a9910f7df238467fb
inka3inka <inka3inka@gmail.com>
2019-12-16 23:14:12 +0100

    About us section styled

---
+++ src/components/aboutUsSection/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function AboutUs() {
+  return (
+    <div className="aboutUs__container">
+      <div className="aboutUs__element--text">
+        <div className="element--text--header">
+          O nas
+        </div>
+        <div className="element--text--description">
+          Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+        </div>
+      </div>
+      <div className="aboutUs__element--picture"/>
+    </div>
+  )
+}
