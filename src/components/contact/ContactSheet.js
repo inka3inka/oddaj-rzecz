759af6b3e0113f20102ce0806036a94037a3b77b
inka3inka <inka3inka@gmail.com>
2020-01-06 14:21:23 +0100

    Basic form style

--- src/components/contact/ContactSheet.js
+++ src/components/contact/ContactSheet.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 export function ContactSheet() {
   return(
-    <>
+    <div className="contactSheet__container">
       <div className="contactSheet__image"/>
-    <div>
-      <div>Skontaktuj się z nami</div>
-      <div>
-        <div>
-          <p>Wpisz swoje imię</p><input/>
-          <p>Wpisz swój e-mail</p><input/>
+      <div className="contactSheet__text">
+        <div className="contactSheet__text--header">Skontaktuj się z nami</div>
+        <div className="contactSheet__text--form">
+          <div className="contactSheet__form--information">
+            <p>Wpisz swoje imię<input placeholder="Krzysztof"/></p>
+            <p>Wpisz swój e-mail<input placeholder="abc@xyz.pl"/></p>
+          </div>
+          <div className="contactSheet__form--message">
+            <p>Wpis swoją wiadomość</p><input/>
+          </div>
         </div>
         <div>
-          <p>Wpis swoją wiadomość</p><input/>
+          <button>Wyślij</button>
         </div>
-      </div>
-      <div>
-        <button>Wyślij</button>
       </div>
     </div>
-    </>
   )
 }
